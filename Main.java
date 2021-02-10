ad3fddaa9f01582bbf615e0917ecc7b937fce010
Sethadleta <adletash@mail.uc.edu>
2021-02-10 00:17:18 +0000

    matched teammates code

--- Main.java
+++ Main.java
@@ -1,81 +1,37 @@
-import java.util.Random;
-//import java.util.Scanner;
+/*
+Lab 4
+Partners: Adam James and Seth Adleta
+Date: 2/9/2021
+*/
 
+import java.util.Random;
+
+import javax.lang.model.util.ElementScanner6;
 
 class Main {
   public static void main(String[] args) {
-    //Scanner s = new Scanner(System.in);
     Random r = new Random();
+    int randomNum = r.nextInt(51);
 
-    int randomInt = r.nextInt(51);
-    System.out.println("The random number is: " + randomInt);
-    
-    int n = randomInt;
-    
-    if(randomInt <= 15)
-    {
-      System.out.println("Ahoy mateys!");
-    }
-    
-    if(randomInt > 20)
-    {
-      System.out.println("Cannonball!");
-    }
-    else if(randomInt < 42)
-    {
-      System.out.println("Cannonball!");
-    }
-    
-    if(randomInt > 42)
-    {
-      System.out.println("Look, a sea shaty!");
-    }
-    
-    while(randomInt >= 0)
-    {
-      System.out.println(randomInt);
-      randomInt--;
-    }
-    /*
-    if(randomInt > 0)
-    {
-      System.out.println("Ahoy mateys!");
-    }
-    else if(randomInt <= 15)
-    {
-      System.out.println("Ahoy mateys!");
-    }
-    
-    if(randomInt > 20)
-    {
-      System.out.println("Cannonball!");
+    System.out.println("The random number is: " + randomNum);
+
+    int originalRand = randomNum;
+
+    while (randomNum >= 0)
+    {System.out.println(randomNum);
+    randomNum--;
     }
 
-    else if(randomInt < 42)
-    {
-      System.out.println("Cannonball!");
-    }
-    
-    if(randomInt > 42)
-    {
-      System.out.println("Look, a sea shaty!");
-    }
-    /*
-    if(randomInt <= 15)
-    {
-      System.out.println("Ahoy mateys!");
-    }
-    
-    else if
-    {
-      20 < randomInt < 42;
-      System.out.print("Cannonball!");
-    }
-    /*
-    else(randomInt > 42);
-    {
-      System.out.println("Look, a sea shaty!");
-    }
-    */
+
+    if (originalRand <= 15)
+    System.out.println("Ahoy Mateys!");
+    else if (originalRand > 20 && originalRand < 42)
+    System.out.println("Cannonball!");
+    else 
+    System.out.println("Look, a sea shanty!");
+
+
+
+
   }
 }
